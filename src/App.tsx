1d95fcfc7cfc41bf38415116be895fb1756957a3
seows <seows@laivdata.com>
2021-12-29 01:18:45 +0900

    버튼 클릭 후 파일 선택기에서 이미지 선택시 미리보기 구현 & 이미지 클릭 시 다시 파일 선택 버튼 노출되도록 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,60 @@
 import React from 'react';
 import styles from './App.module.css';
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 function App() {
-  const fileInputRef = useRef();
+  const fileInputRef = useRef<HTMLInputElement>();
+  const [image, setImage] = useState<File>();
+  const [preview, setPreview] = useState<string>();
+
+  useEffect(() => {
+    if (image) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result as string);
+      };
+      reader.readAsDataURL(image);
+    } else {
+      setPreview(null);
+    }
+  }, [image]);
   return (
     <div className={styles.container}>
       <form className={styles.form}>
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            if (fileInputRef.current) fileInputRef.current.click();
+        {preview ? (
+          <div
+            style={{ backgroundImage: `url(${preview})` }}
+            className={styles.backgroundImage}
+            onClick={() => {
+              setImage(null);
+            }}
+          />
+        ) : (
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              fileInputRef.current.click();
+            }}
+            className={styles.button}
+          >
+            choose image
+          </button>
+        )}
+
+        <input
+          accept="image/*"
+          className={styles.input}
+          type="file"
+          ref={fileInputRef}
+          onChange={(e) => {
+            const file = e.target.files[0];
+            if (file) {
+              setImage(file);
+            } else {
+              setImage(null);
+            }
           }}
-          className={styles.button}
-        >
-          choose image
-        </button>
-        <input className={styles.input} type="file" ref={fileInputRef} />
+        />
       </form>
     </div>
   );
